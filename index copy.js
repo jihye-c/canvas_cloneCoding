cbf3d8e88c4368dc705c5b030eb00fb29f69c91a
jihye-c <jihyec22@gmail.com>
2023-08-10 22:57:25 +0900

    animate particle

--- index copy.js
+++ index copy.js
@@ -1,36 +1,24 @@
+//화면설정
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-// console.log(ctx);
 const dpr = window.devicePixelRatio;
 const canvasWidth = innerWidth;
 const canvasHeight = innerHeight;
-
-//css size - drawn in html
 canvas.style.width = canvasWidth + 'px';
 canvas.style.height = canvasHeight + 'px';
-
-//canvas origin size
-//some monitors have a device pixel ratio value of 2. multiply by dpr to improve resolution.
 canvas.width = canvasWidth * dpr
 canvas.height = canvasHeight * dpr
-
-// if the canvas size is smaller than css size, 
-// the resolution is reduced because the canvas size is increased to css size.
-
-//dpr 사이즈가 2라면 canvas 사이즈 아니고 canvas에 그린 object 사이즈도 2배 해줘야한다.
 ctx.scale(dpr, dpr);
-// ctx.fillRect(10,10,50,50);
 
+//particle 클래스
 class Particle {
     constructor(x,y,radius,vy){
         this.x = x
         this.y = y
         this.radius = radius
         this.vy = vy
-        this.acc = 1.035; //acceleration = 양수, friction = 음수
     }
     update(){
-        this.y *= this.acc;
         this.y += this.vy;
     }
     draw(){
@@ -44,16 +32,14 @@ class Particle {
     }
 }
 
-const x = 100;
-const y = 100;
-const radius = 50;
-const particle = new Particle(x,y,radius);
-const total = 30;
+//minmax 사이에서 랜덤 뽑기
 const randomNumBetween = (min, max) => {
     return Math.random() * (max - min + 1) + min;
 }
-let particles = [];
 
+//particles 배열에 particle 넣어주기
+const total = 30;
+let particles = [];//
 for(let i = 0; i < total; i ++){
     const x = randomNumBetween(0, canvasWidth);
     const y = randomNumBetween(0, canvasHeight);
@@ -62,26 +48,27 @@ for(let i = 0; i < total; i ++){
     const particle  = new Particle(x, y, radius, vy);
     particles.push(particle);
 }
-// console.log(particles);
 
+//애니메이션 변수 설정
 let interval = 1000 / 60;
 let now, delta;
 let then = Date.now();
 
+//애니메이션 함수
 function animate(){
 
-    //매 프레임마다 하나 실행 후 다시 animate 불러와서 실행
-    window.requestAnimationFrame(animate); //화면 주사율기준 (144hz라면 1초에 144번 실행) 
+    window.requestAnimationFrame(animate);
     now = Date.now();
     delta = now - then;
+
     if (delta < interval) return
 
-    ctx.clearRect(0,0,canvasWidth,canvasHeight); //화면 지워줌
+    ctx.clearRect(0,0,canvasWidth,canvasHeight);
 
     particles.forEach(particle => {
+
         particle.update();
         particle.draw();
-
         if(particle.y- particle.radius > canvas.height){
             particle.y = -particle.radius;
             particle.x = randomNumBetween(0, canvasWidth);
@@ -92,4 +79,4 @@ function animate(){
     then = now - (delta % interval);
     
 }
-animate();
+// animate();
