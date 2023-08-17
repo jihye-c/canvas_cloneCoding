49fc5a330c1e14b8bf026be45c534e53b31ed55e
jihye-c <jihyec22@gmail.com>
2023-08-18 00:18:36 +0900

    make particles rounded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
-import {randomNumBetween} from './js/utils.js';
+import {hypotenuse, randomNumBetween} from './js/utils.js';
 
 class Canvas extends CanvasOption{
     constructor(){
@@ -22,10 +22,12 @@ class Canvas extends CanvasOption{
         const x = randomNumBetween(0, this.canvasWidth);
         const y = randomNumBetween(0, this.canvasHeight);
         for (let i = 0; i < PARTICLE_NUM; i++){
-            const vx = randomNumBetween(-5, 5);
-            const vy = randomNumBetween(-5,5)
-            this.particles.push(new Particle(x, y, vx, vy));
-            
+            const r = randomNumBetween(2,100) * hypotenuse(innerWidth, innerHeight) * 0.00012;
+            const angle = Math.PI / 180 * randomNumBetween(0, 360);
+            const vx = r * Math.cos(angle);
+            const vy = r * Math.sin(angle);
+            const opacity = randomNumBetween(0.6, 0.9);
+            this.particles.push(new Particle(x, y, vx, vy, opacity));
         }
     }
 
@@ -41,7 +43,7 @@ class Canvas extends CanvasOption{
         
             if(delta < this.interval) return;
 
-            this.ctx.fillStyle = this.bgColor
+            this.ctx.fillStyle = this.bgColor + '40'
             this.ctx.fillRect(0,0,this.canvasWidth,this.canvasHeight);
         
             this.particles.forEach((particle, index) => {
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,6 @@
 export const randomNumBetween = (min, max) => {
     return Math.random() * (max - min ) + min
 }
+export const hypotenuse = (x, y) => {
+    return Math.sqrt(Math.pow(x,2) + Math.pow(y, 2));
+}
